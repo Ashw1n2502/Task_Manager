79af8e7a2873ff772c83273a98b0a49a827d567b
Ashw1n2502 <129897375+Ashw1n2502@users.noreply.github.com>
2024-03-11 21:22:05 +0000

    Added functionality of task statistics generation

--- task_manager.py
+++ task_manager.py
@@ -35,7 +35,9 @@ for t_str in task_data:
     curr_t['description'] = task_components[2]
     curr_t['due_date'] = datetime.strptime(task_components[3], DATETIME_STRING_FORMAT)
     curr_t['assigned_date'] = datetime.strptime(task_components[4], DATETIME_STRING_FORMAT)
-    curr_t['completed'] = True if task_components[5] == "Yes" else False
+    curr_t['completed'] = True if task_components[5] == "yes" else False
+    curr_t['task_number'] = task_components[6]
+    
 
     task_list.append(curr_t)
 
@@ -109,7 +111,7 @@ def reg_user():
                 print("Passwords do not match, please try again.")
         
             
-def add_task():
+def add_task(task_complete= "no"):
     task_username = input("Name of person assigned to task: ")
     while task_username not in username_password.keys():
         print("User does not exist. Please enter a valid username")
@@ -128,7 +130,9 @@ def add_task():
 
     # Then get the current date.
     curr_date = date.today()
-    task_complete = False
+
+    count = input("Task number : ")
+    
     ''' Add the data to the file task.txt and
         Include 'No' to indicate if the task is complete.'''
     new_task = {
@@ -137,7 +141,8 @@ def add_task():
         "description": task_description,
         "due_date": due_date_time,
         "assigned_date": curr_date,
-        "completed": task_complete
+        "completed": task_complete,
+        "task_number": count
     }
 
     task_list.append(new_task)
@@ -150,7 +155,8 @@ def add_task():
                 t['description'],
                 t['due_date'].strftime(DATETIME_STRING_FORMAT),
                 t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
-                "Yes" if t['completed'] else "No"
+                "yes" if t['completed'] else "no",
+                t['task_number']
             ]
             task_list_to_write.append(";".join(str_attrs))
         task_file.write("\n".join(task_list_to_write))
@@ -159,7 +165,7 @@ def add_task():
 def view_all():
     for t in task_list:
         print(line)
-        disp_str = f"Task: \t\t {t['title']}\n"
+        disp_str = f"Task {t['task_number']}: \t\t {t['title']}\n"
         disp_str += f"Assigned to: \t {t['username']}\n"
         disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
         disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
@@ -172,11 +178,11 @@ def view_mine():
            format of Output 2 presented in the task pdf (i.e. includes spacing
            and labelling)
         '''
-    task_num = 1
+  
     for t in task_list:
         if t['username'] == curr_user:
             print(line)
-            disp_str = f": \t {t['title']}\n"
+            disp_str = f": Task {t['task_number']}\t {t['title']}\n"
             disp_str += f"Assigned to: \t {t['username']}\n"
             disp_str += f"Date Assigned: \t {t['assigned_date'].strftime(DATETIME_STRING_FORMAT)}\n"
             disp_str += f"Due Date: \t {t['due_date'].strftime(DATETIME_STRING_FORMAT)}\n"
@@ -185,17 +191,86 @@ def view_mine():
             print(disp_str)
 
     print("To modify completion status on a task, enter the task number otherwise enter -1 to return to main menu.")
-    user_task_selection = int(input("Please enter the task number to modify : "))
-    if user_task_selection == -1:
-        print("Return to Main Menu")
-    # elif user_task_selection == :
-        # task_components[5]= "Yes"
+    user_task_selection = input("Please enter the task number to complete : ")
+    if user_task_selection == t['task_number']:
+        t['completed'] = "yes"
+        with open("tasks.txt", "w") as task_file:
+            task_list_to_write = []
+            for t in task_list:
+                str_attrs = [
+                    t['username'],
+                    t['title'],
+                    t['description'],
+                    t['due_date'].strftime(DATETIME_STRING_FORMAT),
+                    t['assigned_date'].strftime(DATETIME_STRING_FORMAT),
+                    "yes" if t['completed'] else "No",
+                    t['task_number']
+                ]
+                task_list_to_write.append(";".join(str_attrs))
+            task_file.write("\n".join(task_list_to_write))
+        print("Task successfully completed.")
+    elif user_task_selection == -1:
+        pass
+    else:
+        print("You have made an incorrect selection, please try again")
+        pass
 
 def gen_reports():
-    with open('task_overview.txt', 'w') as gen_file:
-        gen = []
+    tasks = []
+    users = {}
+    completed_tasks = 0
+    overdue_tasks = 0
+
+    with open('tasks.txt', 'r') as file:
+        for line in file:
+            task_data = line.strip().split(';')
+            due_date = datetime.strptime(task_data[3], DATETIME_STRING_FORMAT)
+            completed = task_data[5] == "yes"
+            tasks.append({'assigned_to': task_data[0], 'due_date': due_date, 'completed': completed})
+
+            # Counting the complete and overdue tasks
+            if completed:
+                completed_tasks += 1
+            if not completed and due_date < datetime.now():
+                overdue_tasks += 1
+
+            if task_data[0] in users:
+                users[task_data[0]]['total_tasks'] += 1
+                if completed:
+                    users[task_data[0]]['completed_tasks'] += 1
+            else:
+                users[task_data[0]] = {'total_tasks': 1, 'completed_tasks': 1 if completed else 0}
+
+    total_tasks = len(tasks)
+    uncompleted_tasks = total_tasks - completed_tasks
+
+    # Writing to task_overview.txt
+    with open('task_overview.txt', 'w') as file:
+        file.write(f"Total number of tasks: {total_tasks}\n")
+        file.write(f"Total number of completed tasks: {completed_tasks}\n")
+        file.write(f"Total number of uncompleted tasks: {uncompleted_tasks}\n")
+        file.write(f"Total number of tasks that have not been completed and are overdue: {overdue_tasks}\n")
+        file.write(f"Percentage of tasks that are incomplete: {uncompleted_tasks / total_tasks * 100}%\n")
+        file.write(f"Percentage of tasks that are overdue: {overdue_tasks / total_tasks * 100}%\n")
+
+    with open('user.txt', 'r') as file:
+        total_users = sum(1 for line in file)
+
+    # Writing to user_overview.txt
+    with open('user_overview.txt', 'w') as file:
+        file.write(f"Total number of users registered: {total_users}\n")
+        file.write(f"Total number of tasks that have been generated and tracked: {total_tasks}\n")
         
-    
+        for user, data in users.items():
+            user_total_tasks = data['total_tasks']
+            user_completed_tasks = data['completed_tasks']
+            user_percentage_of_total = (user_total_tasks / total_tasks) * 100 if total_tasks > 0 else 0
+            user_percentage_completed = (user_completed_tasks / user_total_tasks) * 100 if user_total_tasks > 0 else 0
+            
+            file.write(f"\nUser: {user}\n")
+            file.write(f"Total number of tasks assigned: {user_total_tasks}\n")
+            file.write(f"Percentage of tasks assigned to user: {user_percentage_of_total}%\n")
+            file.write(f"Percentage of tasks completed by user: {user_percentage_completed}%\n")
 
 while True:
     # presenting the menu to the user and
@@ -229,12 +304,16 @@ e - Exit
     elif menu == 'ds' and curr_user == 'admin': 
         '''If the user is an admin they can display statistics about number of users
             and tasks.'''
-        num_users = len(username_password.keys())
-        num_tasks = len(task_list)
-
         print(line)
-        print(f"Number of users: \t\t {num_users}")
-        print(f"Number of tasks: \t\t {num_tasks}")
+        print("\nTask Overview:\n")
+        with open('task_overview.txt', 'r') as file:
+            for line in file:
+                print(line.strip())
+        print(line)
+        print("\nUser Overview:\n")
+        with open('user_overview.txt', 'r') as file:
+            for line in file:
+                print(line.strip())
         print(line)    
 
     elif menu == 'e':
